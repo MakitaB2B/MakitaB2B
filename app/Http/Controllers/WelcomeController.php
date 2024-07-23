0c993a98846ad353b1bdd01596b8aa965102025d
MakitaB2B <114991391+JeanLobo1@users.noreply.github.com>
2024-07-23 13:56:55 +0530

    save and fetch from s3

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use League\Flysystem\AwsS3V3\PortableVisibilityConverter;
-
+use App\Models\Test;
 
 class WelcomeController extends Controller
 {
@@ -16,23 +16,42 @@ class WelcomeController extends Controller
     }
 
 
-    public function image(Request $request){
+    public function image(Request $request) {
+        $file1 = $request->file('image1');
+        $file2 = $request->file('image2');
 
-        if ($request["avatar"]) {
-            // Get the uploaded file
-            $file = $request["avatar"];
-    
-                // Get the original file name
-                $originalName = $file->getClientOriginalName();
-    
-                // Store the file (for example, in the 'uploads' directory)
-                $path = Storage::disk('s3')->putFileAs('uploads', $file, $originalName);
-    
-                // Further processing...
-        }
 
+        $name=time().$file1->getClientOriginalName();
+        $name2=time().$file2->getClientOriginalName();
+
+        $path1='/uploads/'.$name;
+        $path2='/uploads/'.$name2;
+
+        Storage::disk('s3')->put($path1,file_get_contents($file1));
+        Storage::disk('s3')->put($path2,file_get_contents($file2));
+     
+      
+        $test=Test::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'phone' => $request->phone,
+            'image1' => Storage::disk('s3')->url($path1),
+            'image2' => Storage::disk('s3')->url($path2),
+        ]);
+         
+     
+    
         return redirect("/");
     }
+    
+
+    public function show($id,Request $request){
+
+        $test=Test::where('id',$id)->select("name","email","phone","image1","image2")->first();
+        return view('singlepage',["test"=> $test]);
+    // return Storage::disk('s3')->response('images/' . $image->filename);
+
+    }    
    
  
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,27 +3,47 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
-<form method="POST" action="/welocome" enctype="multipart/form-data">
+
+<form action="/welocome" method="POST" enctype="multipart/form-data">
 @csrf
-<div class="row mb-3">
-    <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('Avatar') }}</label>
+  <div class="m-5">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Name</label>
+    <input type="text" name="name" class="form-control" id="" aria-describedby="" placeholder="Name">
+  </div>
 
-    <div class="col-md-6">
-        <input id="avatar" type="file" class="form-control @error('avatar') is-invalid @enderror" name="avatar" required autocomplete="avatar">
+  <div class="form-group">
+    <label for="exampleInputEmail1">Email</label>
+    <input type="email" name="email" class="form-control" id="" aria-describedby="" placeholder="Email">
+  </div>
 
-        @error('avatar')
-            <span class="invalid-feedback" role="alert">
-                <strong>{{ $message }}</strong>
-            </span>
-        @enderror
-        <input type="submit" value="Submit">
-        
-    </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Phone</label>
+    <input type="text" name="phone" class="form-control" id="" placeholder="Phone">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlFile1">Image1</label>
+    <input type="file" name="image1" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+
+  <div class="form-group">
+    <label for="exampleFormControlFile1">Image2</label>
+    <input type="file" name="image2" class="form-control-file" id="exampleFormControlFile1">
+  </div>
+
+  <button type="submit" class="btn btn-primary">Submit</button>
+
 </div>
 
 </form>
+
 </body>
 </html>
