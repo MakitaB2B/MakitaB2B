61e506dde0b31b58d36788fcf4221a283dfbee52
MakitaB2B <114991391+JeanLobo1@users.noreply.github.com>
2025-01-21 17:34:37 +0530

    commit
    commit

--- app/Models/Admin/BilledTransaction.php
+++ app/Models/Admin/BilledTransaction.php
@@ -9,13 +9,16 @@ class BilledTransaction extends Model
 {
     use HasFactory;
     protected $guarded=[];
+    public $timestamps = false;
+
     protected $table = 'billed_transactions';
 
-    public function setBilledQtyAttribute($value){
-        $this->attributes['billed_qty'] = (int) $value;
-    }
-    public function getNameAttribute($value)
-    {
-        return (int) $value;
-    }
+
+    // public function setBilledQtyAttribute($value){
+    //     $this->attributes['billed_qty'] = (int) $value;
+    // }
+    // public function getNameAttribute($value)
+    // {
+    //     return (int) $value;
+    // }
 }
